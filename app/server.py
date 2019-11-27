b969ee28d8b92ecce5505b22c5bca2537936c432
ttkkbb <58208263+ttkkbb@users.noreply.github.com>
2019-11-27 16:18:44 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1Zlwe7rsLKa09EeRoa3uRvfFWITo8ll4s'
-export_file_name = 'export (1).pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1muKAyDwUwjuYFMcVc5dSNIjC0NcsrGKl'
+export_file_name = 'export (2).pkl'
 
-classes = ['Rglass','Rcan','Uwrapper','Rtetrapak','Ucutlery','Rplastic']
+classes = ['javan_myna','pigeon','starling','woodpecker','sparrow','dove','whimbrel','duck']
 path = Path(__file__).parent
 
 app = Starlette()
